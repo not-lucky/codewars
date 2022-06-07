bb71b45e321a6d625f1fbcf4887c111cb4933203
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-06-07 11:33:16 +0550

    Last Sync: 2022-06-07 11:13:16

--- 7 kyu/git1.py
+++ 7 kyu/git1.py
@@ -1,17 +1,17 @@
 from git import Repo
 from datetime import datetime
-from time import time, gmtime
+# from time import time, gmtime
 
 PATH_OF_GIT_REPO = r'S:/Everything/codewars/'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+COMMIT_MESSAGE = "Last Sync: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 def git_push():
-        repo = Repo(PATH_OF_GIT_REPO)
-        repo.git.add(A=True)
-        repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote(name='origin')
-        origin.push()
+    repo = Repo(PATH_OF_GIT_REPO)
+    repo.git.add(A=True)
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='origin')
+    origin.push()
 
 
 git_push()
