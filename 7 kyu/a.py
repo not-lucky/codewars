dda2703be3b4c3fdc83287de8967182cd495a2cf
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-06-08 17:29:51 +0530

    Last Sync: 2022-06-08 17:29:50

--- 7 kyu/a.py
+++
@@ -1,7 +0,0 @@
-def infinite_sequence():
-    num = 0
-    while True:
-        yield num
-        num += 2
-for i in infinite_sequence():
-    print(i)
--- 7 kyu/tempCodeRunnerFile.py
+++ 7 kyu/tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-print
+ll.insert(e4, 3)
+# # Should print(4 now
+# print(ll.get_position(3).value)
+
+# # Test delete
+# ll.delete(1)
+# # Should print(2 now
+# print(ll.get_position(1).value)
+# # Should print(4 now
+# print(ll.get_position(2).value)
+# # Should print(3 now
+# print(ll.get_position(3).value)
