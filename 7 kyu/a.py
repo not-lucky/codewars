7b5086777f311997d9b3ec403fe0a5827b8a4526
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-06-10 12:29:39 +0530

    Last Sync: 2022-06-10 12:29:39

--- 7 kyu/a.py
+++ 7 kyu/a.py
@@ -1,12 +1,23 @@
-import math
+class B:
+    def __init__(self, a):
+        self.a = a
 
+    def __repr__(self):
+        return f'B ({self.a})'
 
-def square_root(n):
-    root = n / 2  # initial guess will be 1/2 of n
-    for _ in range(20):
-        root = (1 / 2) * (root + (n / root))
+    def __str__(self):
+        return f'B with {self.a}'
 
-    return root
+    def __bytes__(self):
+        return self.a.to_bytes(4, byteorder='big')
 
+    def __format__(self, spec):
+        if spec == 'f':
+            return str(self.a)
+        return str(self)
 
-print(dir(math), sep='\n')
+b = B(10)
+print(repr(b))
+print(str(b))
+print(bytes(b))
+print(format(b, 'f'))
