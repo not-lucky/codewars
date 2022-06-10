15d923d120775883c16d2a267ff3b4fbde0329d4
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-06-10 18:00:02 +0530

    Last Sync: 2022-06-10 18:00:02

--- 7 kyu/a.py
+++ 7 kyu/a.py
@@ -1,23 +1,4 @@
-class B:
-    def __init__(self, a):
-        self.a = a
+from collections import OrderedDict
 
-    def __repr__(self):
-        return f'B ({self.a})'
 
-    def __str__(self):
-        return f'B with {self.a}'
-
-    def __bytes__(self):
-        return self.a.to_bytes(4, byteorder='big')
-
-    def __format__(self, spec):
-        if spec == 'f':
-            return str(self.a)
-        return str(self)
-
-b = B(10)
-print(repr(b))
-print(str(b))
-print(bytes(b))
-print(format(b, 'f'))
+OrderedDict
