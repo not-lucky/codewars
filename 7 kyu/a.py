8f9e1f1a53c471b63ce66de630a3b730151e90e1
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-06-11 06:00:01 +0530

    Last Sync: 2022-06-11 06:00:01

--- 7 kyu/a.py
+++ 7 kyu/a.py
@@ -1,12 +1,4 @@
-from timeit import Timer
+from collections import OrderedDict
 
-a = [1] * 1000000
-poop = a.pop
-pop_timer = Timer('poop(0)', 'from __main__ import a')
-print(f'pop_timer: {pop_timer.timeit(number=1000)}')
-a = [1] * 1000000
-remove_timer = Timer('a.remove(1)', 'from __main__ import a')
-print(f'remove_timer: {remove_timer.timeit(number=1000)}')
-a = [1] * 1000000
-del_timer = Timer('del a[0]', 'from __main__ import a')
-print(f'del_timer: {del_timer.timeit(number=1000)}')
+
+OrderedDict
